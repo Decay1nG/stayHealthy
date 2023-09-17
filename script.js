030ca6f20e3fe21be869f74fab0d2db4f4cc315d
decay1ng <decayyy1ng@gmail.com>
2023-09-17 14:31:27 +0300

    hero section + adaptive

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const burger = document.querySelector('.burger__menu');
 burger.addEventListener('click', (event) => {
    burger.closest('.header__container').querySelector('.header__navigation').classList.toggle("header__navigation--active");
 });
+
+// ==========-----------> | | сверху шапка | | <-------------=========
+
+let player = videojs(document.querySelector('.video-js'), {
+   breakpoints: {
+      medium: 500
+   }
+});
+player.breakpoints(true);
+
